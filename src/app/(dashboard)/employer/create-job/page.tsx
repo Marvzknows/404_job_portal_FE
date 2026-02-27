3c4d5873f2e6a666789090b6e2dc7a261f8e864c
marvin <marvzknows03@gmail.com>
2026-02-27 09:12:26 +0800

    create job page header

---
+++ src/app/(dashboard)/employer/create-job/page.tsx
@@ -0,0 +1,11 @@
+import CreateJobHeader from "@/components/Employer/JobListing/CreateJobHeader";
+
+const CreateJobPage = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <CreateJobHeader />
+    </div>
+  );
+};
+
+export default CreateJobPage;
--- src/components/Employer/JobListing/CreateJobHeader.tsx
+++ src/components/Employer/JobListing/CreateJobHeader.tsx
@@ -1,24 +1,24 @@
-import { Plus } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import NavigateButton from "./NavigateButton";
 
-const EmployerJobListingHeader = () => {
+const CreateJobHeader = () => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-col gap-1">
-        <h2 className="text-xl md:text-2xl font-semibold">My Job Postings</h2>
+        <h2 className="text-xl md:text-2xl font-semibold">Post New Job</h2>
         <p className="text-muted-foreground text-xs md:text-sm">
-          Manage and track your job listings
+          Create a new job post here
         </p>
       </div>
 
       <NavigateButton
-        href="/employer/create-job"
-        label="Post New Job"
-        icon={<Plus className="w-5 h-5" />}
+        href="/employer/job-listing"
+        label="Back to job listing"
+        icon={<ArrowLeft className="w-5 h-5" />}
         className="bg-violet-500 hover:bg-violet-700"
       />
     </div>
   );
 };
 
-export default EmployerJobListingHeader;
+export default CreateJobHeader;
