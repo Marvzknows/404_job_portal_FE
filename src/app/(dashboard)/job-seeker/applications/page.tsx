84db07bdd28bc713cc246c2c80d601d42f48e02e
marvin <marvzknows03@gmail.com>
2026-03-03 09:01:23 +0800

    component: jobseeker aplication search filter

--- src/app/(dashboard)/job-seeker/applications/page.tsx
+++ src/app/(dashboard)/job-seeker/applications/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import JobApplicationSearchFilter from "@/components/JobSeeker/JobApplication/JobApplicationSearchFilter";
+import PageHeader from "@/components/PageHeader";
 
 const JobSeekerApplicationPage = () => {
-  return <div>JobSeekerApplicationPage</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <PageHeader
+        headerTitle="My Applications"
+        subHeaderTitle="Track your job application and status"
+      />
+
+      <JobApplicationSearchFilter />
+    </div>
+  );
 };
 
 export default JobSeekerApplicationPage;
